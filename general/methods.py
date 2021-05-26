4e67ead1fcda34c23697c26501aa7b649ac8e42a
Gabriel Zima <gabriel.zima@wp.pl>
2021-05-26 15:13:11 +0200

    RunAsync result return

--- general/methods.py
+++ general/methods.py
@@ -3,5 +3,8 @@ import asyncio
 
 def RunAsync(proc):
     #return asyncio.ensure_future(proc)
-    loop = asyncio.get_event_loop()
-    return loop.run_until_complete(proc)
+    onceRunLoop = asyncio.new_event_loop()
+    result = onceRunLoop.run_until_complete(proc)
+    onceRunLoop.close()
+    return result
+
