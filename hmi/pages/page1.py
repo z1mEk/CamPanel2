f2b7fac9895ec83fd2ed319ffa9f28945fdf3bd6
Gabriel <gabriel.zima@gmail.com>
2023-11-22 15:29:03 +0100

    Update page1.py
    Comment name

--- hmi/pages/page1.py
+++ hmi/pages/page1.py
@@ -7,11 +7,11 @@ async def Show():
     await hmi.client.command('page 1')
 
 class b0(controls.TButton):
-    name = 'page1.b0'
+    #name = 'page1.b0'
     pass
 
 class bt0(controls.TDualStateButton):
-    name = 'page1.bt0'
+    #name = 'page1.bt0'
     
     @classmethod
     async def onRelease(cls):
@@ -19,7 +19,7 @@ class bt0(controls.TDualStateButton):
         pass
 
 class bt1(controls.TDualStateButton):
-    name = 'page1.bt1'
+    #name = 'page1.bt1'
 
     @classmethod
     async def onRelease(cls):
@@ -27,28 +27,28 @@ class bt1(controls.TDualStateButton):
         pass
 
 class bt2(controls.TDualStateButton):
-    name = 'page1.bt2'
+    #name = 'page1.bt2'
     
     @classmethod
     async def onRelease(cls):
         relays.data.relay2.val = cls.val
 
 class bt3(controls.TDualStateButton):
-    name = 'page1.bt3'
+    #name = 'page1.bt3'
 
     @classmethod
     async def onRelease(cls):
         relays.data.relay3.val = cls.val
 
 class bt4(controls.TDualStateButton):
-    name = 'page1.bt4'
+    #name = 'page1.bt4'
 
     @classmethod
     async def onRelease(cls):
         relays.data.relay4.val = cls.val
 
 class bt5(controls.TDualStateButton):
-    name = 'page1.bt5'
+    #name = 'page1.bt5'
 
     @classmethod
     async def onRelease(cls):
