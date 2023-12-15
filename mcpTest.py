f1f0859921a607949f962bbf4e5352f2135ca3c0
z1mEk <gabriel.zima@gmail.com>
2023-12-15 18:29:44 +0100

    devnum = 1

--- mcpTest.py
+++ mcpTest.py
@@ -1,8 +1,4 @@
 import EasyMCP2221
 
-mcp = EasyMCP2221.Device()
+mcp = EasyMCP2221.Device(devnum=1)
 print(f"mcp: {mcp}")
-mcp.set_pin_function(gp1='ADC', gp2="ADC")
-mcp.ADC_config(ref="VDD")
-values = mcp.ADC_read()
-print(values)
