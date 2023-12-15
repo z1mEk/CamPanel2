ea1d5d97c06476a4d7dc70f05232d877e65a9a97
z1mEk <gabriel.zima@gmail.com>
2023-12-15 10:37:38 +0100

    CRLF to LF

--- CamPanel.py
+++ CamPanel.py
@@ -3,10 +3,8 @@
 import nest_asyncio
 nest_asyncio.apply()
 from general import events as generalEvents, plugins_loader
-import daemon
 
-with daemon.DaemonContext():
-    event_loop = nest_asyncio.asyncio.get_event_loop()
-    nest_asyncio.asyncio.ensure_future(generalEvents.onRun(event_loop))
-    nest_asyncio.asyncio.ensure_future(plugins_loader.pluginsInit(event_loop))
-    event_loop.run_forever()
+event_loop = nest_asyncio.asyncio.get_event_loop()
+nest_asyncio.asyncio.ensure_future(generalEvents.onRun(event_loop))
+nest_asyncio.asyncio.ensure_future(plugins_loader.pluginsInit(event_loop))
+event_loop.run_forever()
