91002b5f0cfefc00431d287c28f18060d3803671
z1mEk <gabriel.zima@gmail.com>
2023-12-15 14:00:11 +0100

    comports

--- general/devices.py
+++ general/devices.py
@@ -1,25 +1,15 @@
 import serial.tools.list_ports
 
 def find_usb_device_by_vid_pid(vid_pid):
-    # Parsowanie VID i PID z jednego parametru w postaci "067b:2303"
+    # Get a list of all available ports
     vid, pid = map(lambda x: int(x, 16), vid_pid.split(':'))
 
-    # Uzyskanie listy dostępnych portów szeregowych
     ports = list(serial.tools.list_ports.comports())
 
-    # Iteracja przez każdy port w poszukiwaniu pasującego VID i PID
+    # Iterate through the ports and find the one with matching VID and PID
     for port in ports:
-        print(port.description)
-        try:
-            # Pobranie VID i PID z opisu portu
-            port_vid, port_pid = map(lambda x: int(x, 16), port.description.split())
+        if port.vid == vid and port.pid == pid:
+            return port.device
 
-            # Sprawdzenie, czy port ma oczekiwane VID i PID
-            if port_vid == vid and port_pid == pid:
-                # Znaleziono pasujący port, zwróć jego ścieżkę
-                return port.device
-        except (ValueError, IndexError):
-            pass
-
-    # Jeśli nie znaleziono pasującego portu, zwróć None
+    # If no matching port is found, return None
     return None
