8d0f3c375a53fb3e91be791bab7619774218b61b
z1mEk <gabriel.zima@gmail.com>
2023-12-15 15:31:53 +0100

    change to nest_asyncio sleep

--- CamPanel.py
+++ CamPanel.py
@@ -4,17 +4,15 @@ import nest_asyncio
 nest_asyncio.apply()
 from general import events as generalEvents, plugins_loader
 import threading
-import time
 
 def startCampanel():
     while True:
-        # Your code goes here
         print("CamPanel daemon is running...")
         event_loop = nest_asyncio.asyncio.get_event_loop()
         nest_asyncio.asyncio.ensure_future(generalEvents.onRun(event_loop))
         nest_asyncio.asyncio.ensure_future(plugins_loader.pluginsInit(event_loop))
         event_loop.run_forever()
-        time.sleep(1)  # Adjust the sleep duration as needed
+        nest_asyncio.asyncio.sleep(1)  # Adjust the sleep duration as needed
 
 # Create a daemon thread
 daemon_thread = threading.Thread(target=startCampanel)
@@ -26,6 +24,6 @@ daemon_thread.start()
 # Main thread can continue with other tasks or wait for the daemon to finish
 try:
     while True:
-        time.sleep(1)
+        nest_asyncio.asyncio.sleep(1)
 except KeyboardInterrupt:
     print("Main thread interrupted. Exiting...")
