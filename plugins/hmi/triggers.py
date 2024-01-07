394be0fb628234a60109b86bc31e09ef19693dab
z1mEk <gabriel.zima@gmail.com>
2024-01-07 14:51:08 +0100

    fix triggers

--- plugins/hmi/triggers.py
+++ plugins/hmi/triggers.py
@@ -43,6 +43,8 @@ from plugins.hmi.pages.MainPage import MainPage
 ### definition of call name for nextion's components. "call_back" ###
 components_touch_event = [
         #MainPage
+        {"page_id": 0, "component_id": 0, "touch_event": 0, "call_back": MainPage.onTouch},
+        {"page_id": 0, "component_id": 0, "touch_event": 1, "call_back": MainPage.onRelease},
         {"page_id": 0, "component_id": 5, "touch_event": 0, "call_back": MainPage.btWaterPump.onRelease},
         {"page_id": 0, "component_id": 6, "touch_event": 0, "call_back": MainPage.btACInverter.onRelease},
         {"page_id": 0, "component_id": 7, "touch_event": 0, "call_back": MainPage.btHeater.onRelease},
