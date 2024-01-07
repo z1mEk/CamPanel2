38311d1c1ced1680f5873f409253bf001f595a0a
z1mEk <gabriel.zima@gmail.com>
2024-01-07 17:34:49 +0100

    revers to id

--- plugins/hmi/triggers.py
+++ plugins/hmi/triggers.py
@@ -43,10 +43,10 @@ from plugins.hmi.pages.MainPage import MainPage
 ### definition of call name for nextion's components. "call_back" ###
 components_touch_event = [
         #MainPage
-        {"page_id": MainPage.id, "component_id": MainPage.id, "touch_event": 0, "call_back": MainPage.onRelease},
-        {"page_id": MainPage.id, "component_id": MainPage.id, "touch_event": 1, "call_back": MainPage.onTouch},
-        {"page_id": MainPage.id, "component_id": MainPage.btWaterPump.id, "touch_event": 0, "call_back": MainPage.btWaterPump.onRelease},
-        {"page_id": MainPage.id, "component_id": MainPage.btACInverter.id, "touch_event": 0, "call_back": MainPage.btACInverter.onRelease},
-        {"page_id": MainPage.id, "component_id": MainPage.btHeater.id, "touch_event": 0, "call_back": MainPage.btHeater.onRelease},
-        {"page_id": MainPage.id, "component_id": MainPage.btBoiler.id, "touch_event": 0, "call_back": MainPage.btBoiler.onRelease},
+        {"page_id": 0, "component_id": 0, "touch_event": 0, "call_back": MainPage.onRelease},
+        {"page_id": 0, "component_id": 0, "touch_event": 1, "call_back": MainPage.onTouch},
+        {"page_id": 0, "component_id": 5, "touch_event": 0, "call_back": MainPage.btWaterPump.onRelease},
+        {"page_id": 0, "component_id": 6, "touch_event": 0, "call_back": MainPage.btACInverter.onRelease},
+        {"page_id": 0, "component_id": 7, "touch_event": 0, "call_back": MainPage.btHeater.onRelease},
+        {"page_id": 0, "component_id": 8, "touch_event": 0, "call_back": MainPage.btBoiler.onRelease},
     ]
