c5a529692a3d1b787851b14aa4eb1807f04117a3
z1mEk <gabriel.zima@gmail.com>
2024-01-09 17:30:37 +0100

    fix fileName

--- general/logger.py
+++ general/logger.py
@@ -3,7 +3,7 @@ from logging.handlers import RotatingFileHandler
 from general.configLoader import config
 
 log_formatter = Logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-log_handler = RotatingFileHandler(filename=config.logger.filename, maxBytes=config.logger.maxBytes, backupCount=config.logger.backupCount)
+log_handler = RotatingFileHandler(filename=config.logger.fileName, maxBytes=config.logger.maxBytes, backupCount=config.logger.backupCount)
 log_handler.setFormatter(log_formatter)
 logger = Logging.getLogger()
 logger.setLevel(Logging.INFO)
