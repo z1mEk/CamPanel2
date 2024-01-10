fe80679d72ee484499575d57f172e87a972283ef
z1mEk <gabriel.zima@gmail.com>
2024-01-10 21:14:23 +0100

    best fix

--- plugins/nextionHmi.py
+++ plugins/nextionHmi.py
@@ -31,8 +31,8 @@ class plugin:
             MainPage.tCurrent.txt = (
                 '{:.0f}mA'.format(dalyBms.data.currentFlex) if abs(dalyBms.data.currentMiliAmper) < 100 else
                 '{:.3f}A'.format(dalyBms.data.currentFlex) if abs(dalyBms.data.currentMiliAmper) < 1000 else
-                '{:.2f}A'.format(dalyBms.data.currentFlex) if abs(dalyBms.data.currentMiliAmper) / 1000 < 10 else
-                '{:.1f}A'.format(dalyBms.data.currentFlex) if abs(dalyBms.data.currentMiliAmper) / 1000 < 100 else
+                '{:.2f}A'.format(dalyBms.data.currentFlex) if abs(dalyBms.data.currentMiliAmper) < 10000 else
+                '{:.1f}A'.format(dalyBms.data.currentFlex) if abs(dalyBms.data.currentMiliAmper) < 100000 else
                 '{:.0f}A'.format(dalyBms.data.currentFlex)
             )
             
