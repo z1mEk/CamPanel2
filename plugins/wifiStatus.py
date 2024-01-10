fe0c9c415028def2ac092d3d51821508e0e4ba7c
z1mEk <gabriel.zima@gmail.com>
2024-01-10 18:49:12 +0100

    ifconfig to ip

--- plugins/wifiStatus.py
+++ plugins/wifiStatus.py
@@ -11,18 +11,18 @@ class plugin:
     @classmethod
     def wlanUpDown(cls, val):
         if val == 1:
-            logging.info("sudo ifconfig wlan0 up")
-            subprocess.run(["sudo", "ifconfig", "wlan0", "up"])
+            logging.info("sudo ip link set wlan0 up")
+            subprocess.run(["sudo", "ip", "link", "set", "wlan0", "up"])
         else:
-            logging.info("sudo ifconfig wlan0 down")
-            subprocess.run(["sudo", "ifconfig", "wlan0", "down"])
+            logging.info("sudo ip link set wlan0 down")
+            subprocess.run(["sudo", "ip", "link", "set", "wlan0", "down"])
 
     @classmethod
     def isWlan0Up(cls):
         try:
-            result = subprocess.check_output(["ifconfig", "wlan0"])
+            result = subprocess.check_output(["ip", "link", "show", "wlan0"])
             result_str = result.decode("utf-8")
-            data.wifiStatus = 1 if "UP" in result_str else 0
+            data.wifiStatus = 1 if ",UP," in result_str else 0
         except subprocess.CalledProcessError:
             data.wifiStatus = 0
 
