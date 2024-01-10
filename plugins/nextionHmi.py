f2170dd3d5ea6dbde7dc4af1f28b2c169de3cbe5
z1mEk <gabriel.zima@gmail.com>
2024-01-10 21:32:01 +0100

    fix current

--- plugins/nextionHmi.py
+++ plugins/nextionHmi.py
@@ -29,8 +29,7 @@ class plugin:
             MainPage.tVoltage.txt = '{:.2f}V'.format(dalyBms.data.totalVoltage)
 
             MainPage.tCurrent.txt = (
-                '{:.0f}mA'.format(dalyBms.data.currentFlex) if abs(dalyBms.data.currentMiliAmper) < 100 else
-                '{:.3f}A'.format(dalyBms.data.currentFlex) if abs(dalyBms.data.currentMiliAmper) < 1000 else
+                '{:.0f}mA'.format(dalyBms.data.currentFlex) if abs(dalyBms.data.currentMiliAmper) < 1000 else
                 '{:.2f}A'.format(dalyBms.data.currentFlex) if abs(dalyBms.data.currentMiliAmper) < 10000 else
                 '{:.1f}A'.format(dalyBms.data.currentFlex) if abs(dalyBms.data.currentMiliAmper) < 100000 else
                 '{:.0f}A'.format(dalyBms.data.currentFlex)
