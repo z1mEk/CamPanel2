18b37b8b1fd22177abe780cae25d869e3848358f
z1mEk <gabriel.zima@gmail.com>
2024-01-16 21:52:48 +0100

    rsoc fix

--- plugins/nextionHmi.py
+++ plugins/nextionHmi.py
@@ -24,11 +24,11 @@ class plugin:
     @classmethod
     async def updateBMS(cls, interval):
         while True:
+            print(f"rsoc: {dalyBms.data.RSOC}")
             MainPage.jRSOC.val = dalyBms.data.RSOC
             MainPage.tRSOC.txt = '{:.0f}'.format(dalyBms.data.RSOC)
             MainPage.tVoltage.txt = '{:.2f}V'.format(dalyBms.data.totalVoltage)
             MainPage.tCurrent.txt = '{:.2f}V'.format(dalyBms.data.current)           
-            MainPage.tPvPower.txt = '{:.0f}W'.format(385)
 
             MainPage.jRSOC.pco = (
                 helper.RGB2NextionColour(255, 0, 0) if dalyBms.data.RSOC <= 15 else
