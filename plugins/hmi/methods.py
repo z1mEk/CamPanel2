256dd1375404c16e3d6ba7e7af1ab4818764a13a
z1mEk <gabriel.zima@gmail.com>
2024-01-19 18:31:04 +0100

    test without queue

--- plugins/hmi/methods.py
+++ plugins/hmi/methods.py
@@ -2,93 +2,78 @@ from plugins.hmi import hmi
 from general.logger import logging
 import nest_asyncio
 nest_asyncio.apply()
-#from general.queueManager import QueueManager
 
 async def command(command):
-    #async def commandQueue():
-        try:
-            logging.info(f"methods.command({command})")
-            return await hmi.client.command(command)
-        except Exception as e:
-            logging.error(f"Nextion: {e}")
-    #return QueueManager.enqueue(commandQueue)
+    try:
+        logging.debug(f"methods.command({command})")
+        return await hmi.client.command(command)
+    except Exception as e:
+        logging.error(f"Nextion: {e}")
 
 async def wakeUp():
-    #async def wakeUpQueue():
-        try:
-            logging.info(f"methods.wakeUp()")
-            await hmi.client.wakeup()
-        except Exception as e:
-            logging.error(f"Nextion: {e}")
-    #QueueManager.enqueue(wakeUpQueue)
+    try:
+        logging.debug(f"methods.wakeUp()")
+        await hmi.client.wakeup()
+    except Exception as e:
+        logging.error(f"Nextion: {e}")
 
 async def sleep():
-    #async def sleepQueue():
-        try:
-            logging.info(f"methods.sleep()")
-            await hmi.client.sleep()
-        except Exception as e:
-            logging.error(f"Nextion: {e}")
-    #QueueManager.enqueue(sleepQueue)
+    try:
+        logging.debug(f"methods.sleep()")
+        await hmi.client.sleep()
+    except Exception as e:
+        logging.error(f"Nextion: {e}")
     
 async def reset():
     await command('rest')
 
 async def reconnect():
-    #async def reconnectQueue():
-        try:
-            logging.info(f"methods.reconnect()")
-            await hmi.client.reconnect()
-        except Exception as e:
-            logging.error(f"Nextion: {e}")
-    #QueueManager.enqueue(reconnectQueue)
+    try:
+        logging.debug(f"methods.reconnect()")
+        await hmi.client.reconnect()
+    except Exception as e:
+        logging.error(f"Nextion: {e}")
 
 async def dimmer(value:int, save:bool = False):
-    logging.info(f"methods.dimmer({value}, {save})")
+    logging.debug(f"methods.dimmer({value}, {save})")
     await command('dim{}={}'.format('s' if save else '', value))
 
 async def setTimeToSleep(value:int):
-    logging.info(f"methods.setTimeToSleep({value})")
+    logging.debug(f"methods.setTimeToSleep({value})")
     await command('thsp={}'.format(value))
 
 async def setWakeUpSerial(value:bool = False):
-    logging.info(f"methods.setWakeUpSerial({value})")
+    logging.debug(f"methods.setWakeUpSerial({value})")
     await command('thup={}'.format(0 if value else 1))
 
 async def isSleeping() -> bool:
-    #async def isSleepingQueue():
-        try:
-            ret = await hmi.client.is_sleeping() 
-            logging.info(f"methods.isSleeping() -> {ret}")
-            return ret
-        except Exception as e:
-            logging.error(f"Nextion: {e}")
-    #return QueueManager.enqueue(isSleepingQueue)
+    try:
+        ret = await hmi.client.is_sleeping() 
+        logging.debug(f"methods.isSleeping() -> {ret}")
+        return ret
+    except Exception as e:
+        logging.error(f"Nextion: {e}")
 
 async def getProperty(component:str, property:str):
-    #async def getPropertyQueue():
-        try:
-            ret = await hmi.client.get(f"{component}.{property}")
-            logging.info(f"methods.getProperty({component}, {property}) -> {ret}")
-            return ret
-        except Exception as e:
-            logging.error(f"Nextion: {e}")
-    #return QueueManager.enqueue(getPropertyQueue)
+    try:
+        ret = await hmi.client.get(f"{component}.{property}")
+        logging.debug(f"methods.getProperty({component}, {property}) -> {ret}")
+        return ret
+    except Exception as e:
+        logging.error(f"Nextion: {e}")
 
 async def setProperty(component:str, property:str, val):
-    #async def setPropertyQueue():
-        try:
-            logging.info(f"methods.setProperty({component}, {property}, {val})")
-            await hmi.client.set(f"{component}.{property}", val)
-        except Exception as e:
-            logging.error(f"Nextion: {e}")
-    #QueueManager.enqueue(setPropertyQueue)
+    try:
+        logging.debug(f"methods.setProperty({component}, {property}, {val})")
+        await hmi.client.set(f"{component}.{property}", val)
+    except Exception as e:
+        logging.error(f"Nextion: {e}")
 
 async def show(page_id:int):
-    logging.info(f"methods.show({page_id})")
+    logging.debug(f"methods.show({page_id})")
     await command(f"page {page_id}")
 
 async def sendme() -> int:
-    logging.info(f"methods.sendme()")
+    logging.debug(f"methods.sendme()")
     await command(f"sendme")
     return 0
--- plugins/nextionHmi.py
+++ plugins/nextionHmi.py
@@ -3,7 +3,7 @@ nest_asyncio.apply()
 from plugins.hmi import hmi, helper, methods as methodsHmi
 from plugins.hmi.pages.MainPage import MainPage
 from plugins.hmi.pages.solarWaterPage import solarWaterPage
-from plugins import dalyBms, epeverTracer, waterLevel, relays, temperatures #, wifiStatus, solarWaterHeating
+from plugins import dalyBms, epeverTracer, waterLevel, relays, temperatures , wifiStatus, solarWaterHeating
 from datetime import datetime
 from general.logger import logging
 from general.configLoader import config
@@ -13,95 +13,90 @@ class plugin:
     @classmethod
     async def updateTime(cls, interval):
         while True:
-            if methodsHmi.sendme() == 0 or 1==1:
-                MainPage.tTime.txt = datetime.now().strftime("%-H:%M")
+            MainPage.tTime.txt = datetime.now().strftime("%-H:%M")
             await nest_asyncio.asyncio.sleep(interval)
 
     @classmethod
     async def updateTemperatures(cls, interval):
         while True:
-            if methodsHmi.sendme() == 0 or 1==1:
-                MainPage.tInTemp.txt = '{:.0f}'.format(temperatures.data.inTemp)
-                MainPage.tOutTemp.txt = '{:.0f}'.format(temperatures.data.outTemp)
+            MainPage.tInTemp.txt = '{:.0f}'.format(temperatures.data.inTemp)
+            MainPage.tOutTemp.txt = '{:.0f}'.format(temperatures.data.outTemp)
             await nest_asyncio.asyncio.sleep(interval)
 
     @classmethod
     async def updateDalyBMS(cls, interval):
         while True:
-            if methodsHmi.sendme() == 0 or 1==1:
-                MainPage.jRSOC.val = dalyBms.data.RSOC
-                MainPage.tRSOC.txt = '{:.0f}'.format(dalyBms.data.RSOC)
-                MainPage.tVoltage.txt = '{:.2f}V'.format(dalyBms.data.totalVoltage)
+            MainPage.jRSOC.val = dalyBms.data.RSOC
+            MainPage.tRSOC.txt = '{:.0f}'.format(dalyBms.data.RSOC)
+            MainPage.tVoltage.txt = '{:.2f}V'.format(dalyBms.data.totalVoltage)
 
-                MainPage.tCurrent.txt = (
-                    '{:.0f}mA'.format(dalyBms.data.currentFlex) if abs(dalyBms.data.currentMiliAmper) < 1000 else
-                    '{:.2f}A'.format(dalyBms.data.currentFlex) if abs(dalyBms.data.currentMiliAmper) < 10000 else
-                    '{:.1f}A'.format(dalyBms.data.currentFlex) if abs(dalyBms.data.currentMiliAmper) < 100000 else
-                    '{:.0f}A'.format(dalyBms.data.currentFlex)
-                )
-                
-                MainPage.jRSOC.pco = (
-                    helper.RGB2NextionColour(255, 0, 0) if dalyBms.data.RSOC <= 15 else
-                    helper.RGB2NextionColour(255, 255, 0) if dalyBms.data.RSOC <= 30 else
-                    helper.RGB2NextionColour(0, 255, 0)
-                )
+            MainPage.tCurrent.txt = (
+                '{:.0f}mA'.format(dalyBms.data.currentFlex) if abs(dalyBms.data.currentMiliAmper) < 1000 else
+                '{:.2f}A'.format(dalyBms.data.currentFlex) if abs(dalyBms.data.currentMiliAmper) < 10000 else
+                '{:.1f}A'.format(dalyBms.data.currentFlex) if abs(dalyBms.data.currentMiliAmper) < 100000 else
+                '{:.0f}A'.format(dalyBms.data.currentFlex)
+            )
+            
+            MainPage.jRSOC.pco = (
+                helper.RGB2NextionColour(255, 0, 0) if dalyBms.data.RSOC <= 15 else
+                helper.RGB2NextionColour(255, 255, 0) if dalyBms.data.RSOC <= 30 else
+                helper.RGB2NextionColour(0, 255, 0)
+            )
             
             await nest_asyncio.asyncio.sleep(interval)
 
     @classmethod
     async def updateEpeverTracer(cls, interval):
         while True:
-            if methodsHmi.sendme() == 0 or 1==1:
-                MainPage.tPvVoltage.txt = '{:.0f}V'.format(epeverTracer.data.pv.voltage)
-                MainPage.tPvCurrent.txt = '{:.0f}A'.format(epeverTracer.data.pv.current)
-                MainPage.tPvPower.txt = '{:.0f}W'.format(epeverTracer.data.pv.power)
+            MainPage.tPvVoltage.txt = '{:.0f}V'.format(epeverTracer.data.pv.voltage)
+            MainPage.tPvCurrent.txt = '{:.0f}A'.format(epeverTracer.data.pv.current)
+            MainPage.tPvPower.txt = '{:.0f}W'.format(epeverTracer.data.pv.power)
             await nest_asyncio.asyncio.sleep(interval)
 
     @classmethod
     async def updateWaterLevel(cls, interval):
         while True:
-            if methodsHmi.sendme() == 0 or 1==1:
-                MainPage.jWhiteWater.val = waterLevel.data.whiteWaterLevel 
-                MainPage.jWhiteWater.pco = (
-                    helper.RGB2NextionColour(0, 130, 255) if waterLevel.data.whiteWaterLevel > 20 else
-                    helper.RGB2NextionColour(255, 0, 0)
-                )
-                MainPage.jGrayWater.val = waterLevel.data.greyWaterLevel
-                MainPage.jGrayWater.pco = (
-                    helper.RGB2NextionColour(150, 150, 150) if waterLevel.data.greyWaterLevel < 80
-                    else helper.RGB2NextionColour(255, 0, 0)
-                )
-                MainPage.tWhiteWater.txt = '{:.0f}%'.format(waterLevel.data.whiteWaterLevel)
-                MainPage.tGrayWater.txt = '{:.0f}%'.format(waterLevel.data.greyWaterLevel)
+            MainPage.jWhiteWater.val = waterLevel.data.whiteWaterLevel 
+            MainPage.jWhiteWater.pco = (
+                helper.RGB2NextionColour(0, 130, 255) if waterLevel.data.whiteWaterLevel > 20 else
+                helper.RGB2NextionColour(255, 0, 0)
+            )
+
+            MainPage.jGrayWater.val = waterLevel.data.greyWaterLevel
+            MainPage.jGrayWater.pco = (
+                helper.RGB2NextionColour(150, 150, 150) if waterLevel.data.greyWaterLevel < 80
+                else helper.RGB2NextionColour(255, 0, 0)
+            )
+            MainPage.tWhiteWater.txt = '{:.0f}%'.format(waterLevel.data.whiteWaterLevel)
+            MainPage.tGrayWater.txt = '{:.0f}%'.format(waterLevel.data.greyWaterLevel)
             await nest_asyncio.asyncio.sleep(interval)      
 
     @classmethod
     async def updateDualStateButtonValue(cls, interval):
         while True:
-            if methodsHmi.sendme() == 0 or 1==1:
-                MainPage.btWaterPump.val = relays.data.relay0.val
-                MainPage.btACInverter.val = relays.data.relay1.val
-                MainPage.btHeater.val = relays.data.relay2.val
-                MainPage.btBoiler.val = relays.data.relay3.val
+            MainPage.btWaterPump.val = relays.data.relay0.val
+            MainPage.btACInverter.val = relays.data.relay1.val
+            MainPage.btHeater.val = relays.data.relay2.val
+            MainPage.btBoiler.val = relays.data.relay3.val
             await nest_asyncio.asyncio.sleep(interval) 
 
-    # @classmethod
-    # async def initSolarsolarWaterHeatingDataToPage(cls):
-    #     solarWaterPage.btActive.val = solarWaterHeating.data.activeHeating
-    #     solarWaterPage.btBatRsoc.val = solarWaterHeating.data.RsocControl
-    #     solarWaterPage.btPvVoltage.val = solarWaterHeating.data.pvVoltageControl
-    #     solarWaterPage.btPvPower.val = solarWaterHeating.data.pvPowerControl
-    #     solarWaterPage.btHour.val = solarWaterHeating.data.hourControl
+    @classmethod
+    async def initSolarsolarWaterHeatingDataToPage(cls):
+        solarWaterPage.btActive.val = solarWaterHeating.data.activeHeating
+        solarWaterPage.btBatRsoc.val = solarWaterHeating.data.RsocControl
+        solarWaterPage.btPvVoltage.val = solarWaterHeating.data.pvVoltageControl
+        solarWaterPage.btPvPower.val = solarWaterHeating.data.pvPowerControl
+        solarWaterPage.btHour.val = solarWaterHeating.data.hourControl
 
-    #     solarWaterPage.nOnBatRsoc.val = solarWaterHeating.data.onRsoc
-    #     solarWaterPage.nOffBatRsoc.val = solarWaterHeating.data.offRsoc
-    #     solarWaterPage.nOnPvVoltage.val = solarWaterHeating.data.onPvVoltage
-    #     solarWaterPage.nOffPvVoltage.val = solarWaterHeating.data.offPvVoltage
-    #     solarWaterPage.nPvPower.val = solarWaterHeating.data.minPVPower
+        solarWaterPage.nOnBatRsoc.val = solarWaterHeating.data.onRsoc
+        solarWaterPage.nOffBatRsoc.val = solarWaterHeating.data.offRsoc
+        solarWaterPage.nOnPvVoltage.val = solarWaterHeating.data.onPvVoltage
+        solarWaterPage.nOffPvVoltage.val = solarWaterHeating.data.offPvVoltage
+        solarWaterPage.nPvPower.val = solarWaterHeating.data.minPVPower
         
-    #     solarWaterPage.tOnHour.txt = solarWaterHeating.data.onHour
-    #     solarWaterPage.tOffHour.txt = solarWaterHeating.data.offHour   
-    #     await nest_asyncio.asyncio.sleep(1) 
+        solarWaterPage.tOnHour.txt = solarWaterHeating.data.onHour
+        solarWaterPage.tOffHour.txt = solarWaterHeating.data.offHour   
+        await nest_asyncio.asyncio.sleep(1) 
 
     # @classmethod
     # async def updateSolarsolarWaterHeatingData(cls):
@@ -130,4 +125,4 @@ class plugin:
         event_loop.create_task(cls.updateEpeverTracer(2))
         event_loop.create_task(cls.updateWaterLevel(30))
         event_loop.create_task(cls.updateDualStateButtonValue(1))
-        #event_loop.create_task(cls.initSolarsolarWaterHeatingDataToPage())
+        event_loop.create_task(cls.initSolarsolarWaterHeatingDataToPage())
