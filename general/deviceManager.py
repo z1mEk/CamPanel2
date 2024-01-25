36b4261ceac6c4fdc487e52eb81a3e5c8a0331a0
z1mEk <gabriel.zima@gmail.com>
2024-01-25 17:01:29 +0100

    devicemanager info to debug

--- general/deviceManager.py
+++ general/deviceManager.py
@@ -5,14 +5,14 @@ class device():
     def FindUsbDevice(vid_pid):
 
         if vid_pid[:4] == "/dev":
-            logging.info(f"FindUsbDevice({vid_pid}) -> {vid_pid}")
+            logging.debug(f"FindUsbDevice({vid_pid}) -> {vid_pid}")
             return vid_pid
 
         vid, pid = map(lambda x: int(x, 16), vid_pid.split(':'))
 
         for port in list(list_ports.comports()):
             if port.vid == vid and port.pid == pid:
-                logging.info(f"FindUsbDevice({vid_pid}) -> {port.device}")
+                logging.debug(f"FindUsbDevice({vid_pid}) -> {port.device}")
                 return port.device
 
         return None
