eb723ae18172d3d1a0de75c16209113f01ae8551
z1mEk <gabriel.zima@gmail.com>
2024-01-26 22:05:16 +0100

    test

--- plugins/dieselHeater.py
+++ plugins/dieselHeater.py
@@ -125,27 +125,27 @@ class heater:
             buf = bytearray([0] * 24)
             buf[0] = 0x76 # (118).to_bytes(1, byteorder='big') #Start of Frame - 0x76 for LCD
             buf[1] = 0x16 #(22).to_bytes(1, byteorder='big') #Data Size 24bytes
-            buf[2] = transmitPacket.command.to_bytes(1, byteorder='big') #command
+            buf[2] = transmitPacket.command.to_bytes(1, byteorder='big')[0] #command
             transmitPacket.command = 0 # reset command to 0x00
-            buf[3] = transmitPacket.tempSensor.to_bytes(1, byteorder='big') if transmitPacket.thermostatMode == 1 else 0 #temp sensor
-            buf[4] = transmitPacket.tempDesired.to_bytes(1, byteorder='big') #desired temp
-            buf[5] = transmitPacket.pumpFreqMin.to_bytes(1, byteorder='big') #Minimum Pump frequency
-            buf[6] = transmitPacket.pumpFreqMax.to_bytes(1, byteorder='big') #Maximum Pump frequency
-            buf[7] = transmitPacket.funSpeedMin.to_bytes(2, byteorder='big')[0] #Minimum fan speed MSB, LSB
-            buf[8] = transmitPacket.funSpeedMin.to_bytes(2, byteorder='big')[1]
-            buf[9] = transmitPacket.funSpeedMax.to_bytes(2, byteorder='big')[0] #Maximum fan speed MSB, LSB
-            buf[10] = transmitPacket.funSpeedMax.to_bytes(2, byteorder='big')[1]
-            buf[11] = transmitPacket.voltageType.to_bytes(1, byteorder='big')#Heater Operating Voltage 
-            buf[12] = transmitPacket.fanspeedSensor.to_bytes(1, byteorder='big') #Fan speed sensor
-            buf[13] = (50).to_bytes(1, byteorder='big') if transmitPacket.thermostatMode == 1 else (205).to_bytes(1, byteorder='big') #Thermostat/Fixed mode, buf[3] = 0 when fixed mode
-            buf[14] = transmitPacket.tempDesiredMin.to_bytes(1, byteorder='big') #Lower temperature limit
-            buf[15] = transmitPacket.tempDesiredMax.to_bytes(1, byteorder='big') #Upper temperature limit
-            buf[16] = transmitPacket.glowPlugPower.to_bytes(1, byteorder='big') #Glow Plug Power
-            buf[17] = transmitPacket.manualPump.to_bytes(1, byteorder='big') #Manual pump (fuel prime) 0x5A
-            buf[18] = (235).to_bytes(1, byteorder='big')
-            buf[19] = (71).to_bytes(1, byteorder='big') #unknown 0xEB MSB and 0x47 LSB for LCD controller
-            buf[20] = transmitPacket.altitude.to_bytes(2, byteorder='big')[0] #Altitude MSB, LSB
-            buf[21] = transmitPacket.altitude.to_bytes(2, byteorder='big')[1]
+            # buf[3] = transmitPacket.tempSensor.to_bytes(1, byteorder='big') if transmitPacket.thermostatMode == 1 else 0 #temp sensor
+            # buf[4] = transmitPacket.tempDesired.to_bytes(1, byteorder='big') #desired temp
+            # buf[5] = transmitPacket.pumpFreqMin.to_bytes(1, byteorder='big') #Minimum Pump frequency
+            # buf[6] = transmitPacket.pumpFreqMax.to_bytes(1, byteorder='big') #Maximum Pump frequency
+            # buf[7] = transmitPacket.funSpeedMin.to_bytes(2, byteorder='big')[0] #Minimum fan speed MSB, LSB
+            # buf[8] = transmitPacket.funSpeedMin.to_bytes(2, byteorder='big')[1]
+            # buf[9] = transmitPacket.funSpeedMax.to_bytes(2, byteorder='big')[0] #Maximum fan speed MSB, LSB
+            # buf[10] = transmitPacket.funSpeedMax.to_bytes(2, byteorder='big')[1]
+            # buf[11] = transmitPacket.voltageType.to_bytes(1, byteorder='big')#Heater Operating Voltage 
+            # buf[12] = transmitPacket.fanspeedSensor.to_bytes(1, byteorder='big') #Fan speed sensor
+            # buf[13] = (50).to_bytes(1, byteorder='big') if transmitPacket.thermostatMode == 1 else (205).to_bytes(1, byteorder='big') #Thermostat/Fixed mode, buf[3] = 0 when fixed mode
+            # buf[14] = transmitPacket.tempDesiredMin.to_bytes(1, byteorder='big') #Lower temperature limit
+            # buf[15] = transmitPacket.tempDesiredMax.to_bytes(1, byteorder='big') #Upper temperature limit
+            # buf[16] = transmitPacket.glowPlugPower.to_bytes(1, byteorder='big') #Glow Plug Power
+            # buf[17] = transmitPacket.manualPump.to_bytes(1, byteorder='big') #Manual pump (fuel prime) 0x5A
+            # buf[18] = (235).to_bytes(1, byteorder='big')
+            # buf[19] = (71).to_bytes(1, byteorder='big') #unknown 0xEB MSB and 0x47 LSB for LCD controller
+            # buf[20] = transmitPacket.altitude.to_bytes(2, byteorder='big')[0] #Altitude MSB, LSB
+            # buf[21] = transmitPacket.altitude.to_bytes(2, byteorder='big')[1]
 
             #crc = modbusCRC.calculateCrc16(buf[0:21])
             #buf[22], buf[23] = crc & 0xFF, crc >> 8
