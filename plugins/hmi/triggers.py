de08c74491f0b99eb4a3131f120231bb0b443c78
z1mEk <gabriel.zima@gmail.com>
2024-01-26 19:47:52 +0100

    triggers

--- plugins/hmi/triggers.py
+++ plugins/hmi/triggers.py
@@ -1,4 +1,5 @@
 from plugins.hmi.pages.mainPage import mainPage
+from plugins.hmi.pages.dieselHeaterPage import dieselHeaterPage
 
 ### definition of call name for nextion's components. "call_back" ###
 components_touch_event = [
@@ -9,4 +10,6 @@ components_touch_event = [
         {"page_id": 0, "component_id": 6, "touch_event": 0, "call_back": mainPage.btACInverter.onRelease},
         {"page_id": 0, "component_id": 7, "touch_event": 0, "call_back": mainPage.btHeater.onRelease},
         {"page_id": 0, "component_id": 8, "touch_event": 0, "call_back": mainPage.btBoiler.onRelease},
+
+        {"page_id": 2, "component_id": 8, "touch_event": 0, "call_back": mainPage.btBoiler.onRelease},
     ]
