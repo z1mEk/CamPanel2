3f0eeeb3e600e42a54fe673f086dbf74c08221e7
z1mEk <gabriel.zima@gmail.com>
2024-01-26 20:17:20 +0100

    wyjheb

--- plugins/hmi/pages/dieselHeatPage.py
+++ plugins/hmi/pages/dieselHeatPage.py
@@ -2,7 +2,6 @@ import nest_asyncio
 from nest_asyncio import asyncio
 nest_asyncio.apply()
 from plugins.hmi.controls import TPage, TText, TDualStateButton, TNumber
-from plugins import dieselHeater
 from general.logger import logging
 
 class dieselHeatPage(TPage):
