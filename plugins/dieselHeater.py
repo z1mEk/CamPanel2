b3788b7b6e0b2e6fdbbc06e5a11d1c24b5a9f4b6
z1mEk <gabriel.zima@gmail.com>
2024-01-27 22:54:38 +0100

    test

--- plugins/dieselHeater.py
+++ plugins/dieselHeater.py
@@ -140,6 +140,7 @@ class heater:
     @classmethod
     def translateReceivePacket(cls, buf):
         try:
+            logging.info(f"{buf}")
             cls.runState = int.from_bytes(buf[2], byteorder='big')
             cls.onOff = int.from_bytes(buf[3], byteorder='big')
             cls.supplyVoltage = int.from_bytes(buf[4], byteorder='big') / 10
@@ -169,13 +170,11 @@ class heater:
                 #     cls.srl.open()
 
                 buf_transmit = heater.createTransmitPacket()
-                logging.info(f"DieselHeater buf_transmit = {buf_transmit}")
                 #cls.srl.write(buf_transmit)
                 await asyncio.sleep(0.1)
                 #buf_receive = cls.srl.read(48) # 48?
                 lista = [0,0,5,1,0,15,0,216,0,131,28,0,121,0,25,15,52,56,52,12,21,25,25,12,25,21,25]
                 buf_receive = b''.join(x.to_bytes(1, 'big') for x in lista)
-                logging.info(f"{buf_receive}")
                 heater.translateReceivePacket(buf_receive[:24])
                 cls.lastSend = time.time()
 
