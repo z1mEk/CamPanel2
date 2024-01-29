a0ea368c1fdf90dfbbf7fa9b6d867b894d03b622
z1mEk <gabriel.zima@gmail.com>
2024-01-29 21:57:19 +0100

    test

--- plugins/hmi/pages/dieselHeatPage.py
+++ plugins/hmi/pages/dieselHeatPage.py
@@ -20,7 +20,7 @@ class dieselHeatPage(TPage):
     class btThermostat(TDualStateButton):
         @classmethod
         async def onRelease(cls):
-            dieselHeater.transmitPacket.thermostatMode = cls.val
+            dieselHeater.data.transmitPacket.thermostatMode = cls.val
 
     class tValue(TText):
         pass
