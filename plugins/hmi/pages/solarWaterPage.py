eb0f4e6b3ed364c287e101e09834b53f5423ee4f
z1mEk <gabriel.zima@gmail.com>
2024-01-30 20:05:39 +0100

    test

--- plugins/hmi/pages/solarWaterPage.py
+++ plugins/hmi/pages/solarWaterPage.py
@@ -6,25 +6,7 @@ from general.logger import logging
 from plugins import solarWaterHeating
 
 class solarWaterPage(TPage):
-
-    @classmethod
-    async def onShow(cls):
-        solarWaterPage.btActive.val = solarWaterHeating.data.activeHeating
-        solarWaterPage.btBatRsoc.val = solarWaterHeating.data.RsocControl
-        solarWaterPage.btPvVoltage.val = solarWaterHeating.data.pvVoltageControl
-        solarWaterPage.btPvPower.val = solarWaterHeating.data.pvPowerControl
-        solarWaterPage.btHour.val = solarWaterHeating.data.hourControl
-
-        solarWaterPage.nOnBatRsoc.val = solarWaterHeating.data.onRsoc
-        solarWaterPage.nOffBatRsoc.val = solarWaterHeating.data.offRsoc
-        solarWaterPage.nOnPvVoltage.val = solarWaterHeating.data.onPvVoltage
-        solarWaterPage.nOffPvVoltage.val = solarWaterHeating.data.offPvVoltage
-        solarWaterPage.nPvPower.val = solarWaterHeating.data.minPVPower
-
-        solarWaterPage.tOnHour.txt = solarWaterHeating.data.onHour
-        solarWaterPage.tOffHour.txt = solarWaterHeating.data.offHour          
-        return await super().onShow()
-    
+   
     @classmethod
     async def onExit(cls):
         solarWaterHeating.data.activeHeating = solarWaterPage.btActive.val
--- plugins/nextionHmi.py
+++ plugins/nextionHmi.py
@@ -88,24 +88,24 @@ class plugin:
                 mainPage.btBoiler.val = relays.data.relay3.val
             await asyncio.sleep(interval) 
 
-    # @classmethod
-    # async def initSolarsolarWaterHeatingDataToPage(cls):
-    #     await asyncio.sleep(2)  
-    #     solarWaterPage.btActive.val = solarWaterHeating.data.activeHeating
-    #     solarWaterPage.btBatRsoc.val = solarWaterHeating.data.RsocControl
-    #     solarWaterPage.btPvVoltage.val = solarWaterHeating.data.pvVoltageControl
-    #     solarWaterPage.btPvPower.val = solarWaterHeating.data.pvPowerControl
-    #     solarWaterPage.btHour.val = solarWaterHeating.data.hourControl
+    @classmethod
+    async def initSolarsolarWaterHeatingDataToPage(cls):
+        await asyncio.sleep(2)  
+        solarWaterPage.btActive.val = solarWaterHeating.data.activeHeating
+        solarWaterPage.btBatRsoc.val = solarWaterHeating.data.RsocControl
+        solarWaterPage.btPvVoltage.val = solarWaterHeating.data.pvVoltageControl
+        solarWaterPage.btPvPower.val = solarWaterHeating.data.pvPowerControl
+        solarWaterPage.btHour.val = solarWaterHeating.data.hourControl
 
-    #     solarWaterPage.nOnBatRsoc.val = solarWaterHeating.data.onRsoc
-    #     solarWaterPage.nOffBatRsoc.val = solarWaterHeating.data.offRsoc
-    #     solarWaterPage.nOnPvVoltage.val = solarWaterHeating.data.onPvVoltage
-    #     solarWaterPage.nOffPvVoltage.val = solarWaterHeating.data.offPvVoltage
-    #     solarWaterPage.nPvPower.val = solarWaterHeating.data.minPVPower
+        solarWaterPage.nOnBatRsoc.val = solarWaterHeating.data.onRsoc
+        solarWaterPage.nOffBatRsoc.val = solarWaterHeating.data.offRsoc
+        solarWaterPage.nOnPvVoltage.val = solarWaterHeating.data.onPvVoltage
+        solarWaterPage.nOffPvVoltage.val = solarWaterHeating.data.offPvVoltage
+        solarWaterPage.nPvPower.val = solarWaterHeating.data.minPVPower
 
-    #     solarWaterPage.tOnHour.txt = solarWaterHeating.data.onHour
-    #     solarWaterPage.tOffHour.txt = solarWaterHeating.data.offHour   
-    #     await asyncio.sleep(1) 
+        solarWaterPage.tOnHour.txt = solarWaterHeating.data.onHour
+        solarWaterPage.tOffHour.txt = solarWaterHeating.data.offHour   
+        await asyncio.sleep(1) 
 
     # @classmethod
     # async def updateSolarsolarWaterHeatingData(cls, interval): 
@@ -156,5 +156,5 @@ class plugin:
         event_loop.create_task(cls.updateEpeverTracer(2))
         event_loop.create_task(cls.updateWaterLevel(2))
         event_loop.create_task(cls.updateDualStateButtonValue(1))
-        #event_loop.create_task(cls.updateSolarsolarWaterHeatingData(1))
+        event_loop.create_task(cls.initSolarsolarWaterHeatingDataToPage())
         event_loop.create_task(cls.updateDieselHeaterData(1))
