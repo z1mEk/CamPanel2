f372174ad2a901056e14245e08d9acbbf8825869
z1mEk <gabriel.zima@gmail.com>
2024-01-31 18:10:45 +0100

    test

--- plugins/dieselHeater.py
+++ plugins/dieselHeater.py
@@ -147,7 +147,7 @@ class plugin:
             data.errorDescription = helper.getErrorDescription(data.errorState)
             data.fixedModePumpFreq = frame[19]        
             data.valueDisplay = "OFF" if data.runState == 0 else "{:.0f}°C".format(transmitPacket.tempDesired) \
-                    if transmitPacket.thermostatMode == 1 else "{:.1f}Hz".format(helper.calculateFrequency())
+                    if transmitPacket.thermostatMode == 1 else "{:.1f}Hz".format(helper.calculateFrequency() / 10)
         except Exception as e:
             logging.error(f"dieselHeater - translateReceivePacket: {e}")
 
