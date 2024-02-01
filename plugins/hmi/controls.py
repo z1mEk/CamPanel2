21735061faadce6286c15031fea93180292da788
z1mEk <gabriel.zima@gmail.com>
2024-02-01 18:03:52 +0100

    test

--- plugins/hmi/controls.py
+++ plugins/hmi/controls.py
@@ -51,6 +51,17 @@ class BasePage(type, object):
     
     name = __name__
 
+    def __new__(cls, name, bases, dct):
+        module = sys.modules[dct['__module__']]
+        module_name = os.path.splitext(os.path.basename(module.__file__))[0]
+        base_class_name = bases[0].__name__ if bases else None
+
+        dct["page"] = module_name
+        dct["fullname"] = f"{module_name}"
+        dct["type_name"] = base_class_name
+
+        return super().__new__(cls, name, bases, dct)    
+
     def getAtrr(self, attr):
         return asyncio.run(hmiMethods.getProperty(self.name, attr))
 
