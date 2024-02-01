e17443a259130d2eab09cb1571409aec0a5772bd
z1mEk <gabriel.zima@gmail.com>
2024-02-01 18:08:36 +0100

    test

--- plugins/hmi/controls.py
+++ plugins/hmi/controls.py
@@ -57,21 +57,21 @@ class BasePage(type, object):
         base_class_name = bases[0].__name__ if bases else None
 
         dct["page"] = module_name
-        #dct["fullname"] = f"{module_name}"
+        dct["fullname"] = f"{module_name}"
         dct["type_name"] = base_class_name
 
         return super().__new__(cls, name, bases, dct)    
 
     def getAtrr(self, attr):
-        return asyncio.run(hmiMethods.getProperty(self.page, attr))
+        return asyncio.run(hmiMethods.getProperty(self.fullname, attr))
 
     def setAttr(self, attr, value):
-        asyncio.run(hmiMethods.setProperty(self.page, attr, value))
+        asyncio.run(hmiMethods.setProperty(self.fullname, attr, value))
 
     @classmethod
     async def Show(cls):
         logging.info(f"wlazło w Show")
-        await hmiMethods.showPageName(cls.page)
+        await hmiMethods.showPageName(cls.fullname)
 
 #endregion
         
