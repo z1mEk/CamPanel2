1516fd7f5fed45a279fa1cdd4036c9ef13304b95
z1mEk <gabriel.zima@gmail.com>
2024-02-02 09:58:41 +0100

    test wifistatus

--- plugins/hmi/pages/settingsPage.py
+++ plugins/hmi/pages/settingsPage.py
@@ -2,14 +2,18 @@ import nest_asyncio
 from nest_asyncio import asyncio
 nest_asyncio.apply()
 from plugins.hmi.controls import TPage, TText, TDualStateButton, TNumber
+from plugins import wifiStatus
 from general.logger import logging
 
 class settingsPage(TPage):
 
     @classmethod
     async def onShow(cls):
-        pass
+        cls.btWifi.val = wifiStatus.data.wifiStatus
     
     @classmethod
     async def onExit(cls):      
+        wifiStatus.data.wifiStatus = cls.btWifi.val
+
+    class btWifi(TDualStateButton):
         pass
