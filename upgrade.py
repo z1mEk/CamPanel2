e739927d799ba7759e155d2fa94cb97e50dcf2ff
z1mEk <gabriel.zima@gmail.com>
2024-02-02 19:40:55 +0100

    update hmi

--- upgrade.py
+++ upgrade.py
@@ -3,26 +3,25 @@ import io
 import subprocess
 from general.configLoader import config
 from general.deviceManager import device
-from nextion import Nextion, client
+from nextion import Nextion
 import nest_asyncio
 from nest_asyncio import asyncio
 nest_asyncio.apply()
 
-def git_pull(repo_path):
+def git_pull():
     try:
-        print(f"Pull data from git")
+        print(f"git pull")
         result = subprocess.check_output(["git", "pull"])
         result_str = result.decode("utf-8")
         print(f"{result_str}")
         return True if "NextionInterface.tft" in result_str else False
     except Exception as e:
-        print("git_pull {e}")
+        print("git_pull: {e}")
     return False
 
 async def upload_tft_to_nextion(tft_path):
-    print(f"Read TFT file: {tft_path}")
+    print(f"TFT file update required, this may take a few minutes. Progress will be visible on the Nextion display")
     try:
-
         event_loop = asyncio.get_event_loop()
         nextion_device = device.FindUsbDevice(config.nextion.device)
         nextion_client = Nextion(nextion_device, config.nextion.baudrate, None, event_loop, reconnect_attempts=5, encoding="utf-8")
@@ -30,29 +29,22 @@ async def upload_tft_to_nextion(tft_path):
 
         with open(tft_path, 'rb') as file:
             buffered_reader = io.BufferedReader(file)
-            print(f"Upload file: {tft_path}")
+            print(f"Upload file {tft_path} to Nextion...")
             await nextion_client.upload_firmware(buffered_reader, 115200)
 
-        print(f"File {tft_path} uploaded")
+        print(f"File {tft_path} uploaded to Nextion")
 
     except Exception as e:
         print(f"upload tft file error: {e}")
 
-def eventHandler(type_, data):
-    pass
-
 if __name__ == "__main__":
-
-    # Replace with the path to your tft file
     tft_path = './plugins/hmi/NextionInterface.tft'
 
     print(f"Stop CamPanel.service")
     subprocess.run(['sudo', 'systemctl', 'stop', 'CamPanel.service'])
 
-    # Sprawdzanie, czy są nowe zmiany w repozytorium dla pliku .tft
-    if git_pull("./"):
+    if git_pull():
         asyncio.run(upload_tft_to_nextion(tft_path))
 
-    # Restartowanie określonej usługi
     print(f"Start CamPanel.service")
     subprocess.run(['sudo', 'systemctl', 'start', 'CamPanel.service'])
