2cd8049a39b263db7067ff00f61c1bf808582797
z1mEk <gabriel.zima@gmail.com>
2024-02-03 19:44:54 +0100

    test

--- btserver.py
+++ btserver.py
@@ -1,7 +1,6 @@
 #! /usr/bin/python
 
 import bluetooth
-import uuid
 
 server_socket = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 
@@ -9,9 +8,8 @@ port = 1
 server_socket.bind(("",port))
 server_socket.listen(1)
 
-uuID = 99
 
-bluetooth.advertise_service( server_socket, "test", service_id=uuID )
+bluetooth.advertise_service( server_socket, "test" )
 
 client_socket, client_address = server_socket.accept()
 print(client_socket)
