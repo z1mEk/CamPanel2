4b82857f406e6c3cc026b7e503c8075a5ca1a412
z1mEk <gabriel.zima@gmail.com>
2024-02-04 15:46:38 +0100

    test

--- btserver.py
+++ btserver.py
@@ -11,25 +11,11 @@ def send_json_data(peripheral, characteristic, data):
     json_data = json.dumps(data)
     characteristic.write(json_data.encode())
 
-def get_ble_address():
-    try:
-        result = subprocess.check_output(["hcitool", "dev"]).decode("utf-8")
-        lines = result.split("\n")
-        for line in lines:
-            if "hci" in line:
-                parts = line.split()
-                if len(parts) == 3:
-                    return parts[2]  # Adres MAC urządzenia BLE
-    except Exception as e:
-        print("Błąd podczas pobierania adresu BLE:", e)
-    return None
-
 def main():
     # Ustawienia BLE
     service_uuid = "6e400001-b5a3-f393-e0a9-e50e24dcca9e"  # Przykładowa usługa Custom UART
     characteristic_uuid = "6e400002-b5a3-f393-e0a9-e50e24dcca9e"  # Przykładowa charakterystyka TX
-    print(f"adress = {get_ble_address()}")
-    peripheral_address = get_ble_address()  # Wstaw rzeczywisty adres MAC
+    peripheral_address = "B8:27:EB:40:3E:40"  # Wstaw rzeczywisty adres MAC
     peripheral = Peripheral()  # Utwórz obiekt Peripheral bez podawania adresu na razie
     peripheral.setDelegate(BLEDelegate())
 
