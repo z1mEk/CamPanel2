b7bcf8667b6123c7c911c67927256b2d03713149
z1mEk <gabriel.zima@gmail.com>
2024-02-05 18:44:22 +0100

    test

--- btserver.py
+++ btserver.py
@@ -2,24 +2,29 @@ from bluepy.btle import Peripheral, Characteristic, Service, DefaultDelegate
 
 class BLEServer(Peripheral):
     def __init__(self):
-        Peripheral.__init__(self)
+        try:
+            Peripheral.__init__(self)
+            
+            # Utwórz usługę GATT
+            service_uuid = "0000180a-0000-1000-8000-00805f9b34fb"  # Przykładowa usługa Device Information
+            self.device_info_service = Service(service_uuid, 1)
 
-        # Utwórz usługę GATT
-        service_uuid = "0000180a-0000-1000-8000-00805f9b34fb"  # Przykładowa usługa Device Information
-        self.device_info_service = Service(service_uuid, 1)
+            # Utwórz charakterystykę w ramach usługi
+            characteristic_uuid = "00002a29-0000-1000-8000-00805f9b34fb"  # Przykładowa charakterystyka Manufacturer Name String
+            self.manufacturer_name_characteristic = Characteristic(characteristic_uuid, 2, self.device_info_service, 0)
 
-        # Utwórz charakterystykę w ramach usługi
-        characteristic_uuid = "00002a29-0000-1000-8000-00805f9b34fb"  # Przykładowa charakterystyka Manufacturer Name String
-        self.manufacturer_name_characteristic = Characteristic(characteristic_uuid, 2, self.device_info_service, 0)
+            # Ustaw wartość domyślną
+            self.manufacturer_name_characteristic.write("MyBLEDevice")
 
-        # Ustaw wartość domyślną
-        self.manufacturer_name_characteristic.write("MyBLEDevice")
+            # Dodaj charakterystykę do usługi
+            self.device_info_service.addCharacteristic(self.manufacturer_name_characteristic)
 
-        # Dodaj charakterystykę do usługi
-        self.device_info_service.addCharacteristic(self.manufacturer_name_characteristic)
+            # Dodaj usługę do urządzenia
+            self.addService(self.device_info_service)
 
-        # Dodaj usługę do urządzenia
-        self.addService(self.device_info_service)
+        except Exception as e:
+            print(f"Błąd inicjalizacji serwera BLE: {e}")
+            raise
 
 if __name__ == "__main__":
     try:
