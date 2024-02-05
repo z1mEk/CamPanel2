df92e32883b7ff09007e63ad4fdf131fbec5aea4
z1mEk <gabriel.zima@gmail.com>
2024-02-05 19:13:06 +0100

    test

--- btclient.py
+++ btclient.py
@@ -2,8 +2,8 @@ from bluepy.btle import Peripheral, UUID
 
 def run_ble_client():
     # Ustawienia BLE
-    service_uuid = "f000ffc0-0451-4000-b000-000000000000"
-    characteristic_uuid = "f000ffc1-0451-4000-b000-000000000000"
+    service_uuid = "12345678-1234-5678-1234-56789abcdef0"
+    characteristic_uuid = "12345678-1234-5678-1234-56789abcdef1"
 
     peripheral_address = "02:11:23:34:86:E2"  # Wstaw rzeczywisty adres MAC serwera BLE
 
@@ -16,14 +16,8 @@ def run_ble_client():
         characteristic = service.getCharacteristics(UUID(characteristic_uuid))[0]
 
         # Wyślij zapytanie do serwera
-        characteristic.write(b"Pytanie")
-
-        # Czekaj na odpowiedź od serwera
-        if peripheral.waitForNotifications(10.0):
-            response_data = characteristic.read()
-            print("Otrzymano odpowiedź od serwera:", response_data.decode())
-        else:
-            print("Przekroczono czas oczekiwania na odpowiedź od serwera.")
+        value = characteristic.read()
+        print("Otrzymano odpowiedź od serwera:", value.decode())
 
     except Exception as e:
         print(f"Błąd podczas komunikacji z serwerem: {e}")
