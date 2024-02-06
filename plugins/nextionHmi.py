da3b25e9cc2f06585597cdfff5e17dc5a3d5946e
z1mEk <gabriel.zima@gmail.com>
2024-02-06 18:28:57 +0100

    testet

--- plugins/nextionHmi.py
+++ plugins/nextionHmi.py
@@ -133,7 +133,7 @@ class plugin:
                     dieselHeatPage.vaWent.val = dieselHeater.data.fanRpm
                     dieselHeatPage.vaPump.val = int(dieselHeater.data.actualPumpFreq * 10)
                     dieselHeatPage.vaHeaterTemp.val = dieselHeater.data.heatExchTemp
-                    dieselHeatPage.vaGlowCurrent.val = dieselHeater.data.glowPlugCurrent
+                    dieselHeatPage.vaGlowCurrent.val = int(dieselHeater.data.glowPlugCurrent)
                     
             except Exception as e:
                 logging.error(f"updateDieselHeaterData - {e}")
