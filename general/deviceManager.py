3cc3ad7867cf88e88dbe4399e3dc08010a6444e2
z1mEk <gabriel.zima@gmail.com>
2024-03-19 19:49:25 +0100

    revert

--- general/deviceManager.py
+++ general/deviceManager.py
@@ -4,21 +4,15 @@ from general.logger import logging
 class device():
     def FindUsbDevice(vid_pid):
 
-        logging.info(f"FindUsbDevice({vid_pid})")
-
-        if vid_pid[:4] == "/dev" or vid_pid[:3] == "COM":
-            logging.info(f"FindUsbDevice({vid_pid}) -> {vid_pid}")
+        if vid_pid[:4] == "/dev":
+            logging.debug(f"FindUsbDevice({vid_pid}) -> {vid_pid}")
             return vid_pid
-        
-        devices = vid_pid.split(':')
-        vid = devices[0]
-        pid = devices[1]
-        #if len(devices) > 2:
-        #    snb = devices[3]
+
+        vid, pid = map(lambda x: int(x, 16), vid_pid.split(':'))
 
         for port in list(list_ports.comports()):
-            if port.vid == vid and port.pid == pid: # and (port.serial_number == snb or snb is None):
-                logging.info(f"FindUsbDevice({vid_pid}) -> {port.device}")
+            if port.vid == vid and port.pid == pid:
+                logging.debug(f"FindUsbDevice({vid_pid}) -> {port.device} || {port.serial_number}")
                 return port.device
 
-        return None     
+        return None
